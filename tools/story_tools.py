68c98c9691d68816aa61d196773a230864de92e0
rughvi <rughvi8784@gmail.com>
2026-05-28 23:03:38 +0530

    story check output format

--- tools/story_tools.py
+++ tools/story_tools.py
@@ -4,16 +4,27 @@ from pydantic import BaseModel, Field
 class StoryCheckInput(BaseModel):
     story: int = Field(description="Story number")
 
+class StoryCheckOutput(BaseModel):
+    story: int = Field(description="Story number")
+    exists: bool = Field(description="Story exists")
+    message: str = Field(description="Story exists or not")
+
 class StoryInsertInput(BaseModel):
     story: int = Field(description="Story number")
 
 @tool(args_schema=StoryCheckInput)
-def exists(story: int):
+def exists(story: int) -> StoryCheckOutput:
     """Checks if story exists or not"""
     if story == 189:
-        return "Exists"
+        return StoryCheckOutput(
+            story=story,
+            exists=True,
+            message="Story exists").model_dump()
     else:
-        return "Not exists"
+        return StoryCheckOutput(
+            story=story,
+            exists=False,
+            message="Story not exists").model_dump()
 
 @tool(args_schema=StoryInsertInput)
 def insert(story: int):
